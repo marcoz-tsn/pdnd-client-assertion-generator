1b2e04a0920318e2873e2857fc8d718acd0c0fea
marco.zimello@tecsis.it <marco.zimello@tecsis.it>
2025-04-30 19:24:03 +0200

    added digest names constant

--- src/PDNDClientAssertionGenerator/OAuth2Consts.ts.cs
+++ src/PDNDClientAssertionGenerator/OAuth2Consts.ts.cs
@@ -1,7 +1,11 @@
 public static class OAuth2Consts
 {
+  // Parameter names
+  public const string PDNDValueParameterName = "value";
+
   // Claim names
   public const string PDNDPurposeIdClaimName = "purposeId";
+  public const string PDNDDigestClaimName = "digest";
 
   // Common values
   public const string TokenEndpointUat = "https://auth.uat.interop.pagopa.it/token.oauth2";
